67d7610541bde63f8648e251253be8c792cfaed8
ViktorS01 <75483754+ViktorS01@users.noreply.github.com>
2021-03-08 03:28:25 +0400

    feat: add task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++){
+
+    for (let j = 0; j < bracketsConfig.length; j++){
+
+      if (bracketsConfig[j][0] === bracketsConfig[j][1]){
+        if (str[i] == bracketsConfig[j][0]){
+          if (str[i] == stack[stack.length - 1]){
+            stack.pop();
+            break;
+          }
+          else{
+            stack.push(str[i]);
+            break;
+          }
+        }
+      }
+
+      if ((stack.length == 0 && str[i] == bracketsConfig[j][1])){
+        return false;
+      }
+
+      if (str[i] == bracketsConfig[j][0]){
+        stack.push(str[i]);
+      }
+
+      if (str[i] === bracketsConfig[j][1] && stack.length != 0){
+
+        if (stack[stack.length - 1] == bracketsConfig[j][0]){
+          stack.pop();
+        }
+        else{
+          return false;
+        }
+
+      }
+      
+    }
+
+  }
+
+  if (stack.length != 0){
+    return false;
+  }
+  return true;
 }
